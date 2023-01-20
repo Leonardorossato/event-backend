35f72ab184c98dd337bc0856bf04d52f03a71a79
Leonardorossato <leonardorossato21@gmail.com>
2023-01-20 15:23:04 -0300

    update receivers.controller.ts

--- src/receivers/receivers.controller.ts
+++ src/receivers/receivers.controller.ts
@@ -11,40 +11,45 @@ import { ReceiversService } from './receivers.service';
 import { CreateReceiverDto } from './dto/create-receiver.dto';
 import { UpdateReceiverDto } from './dto/update-receiver.dto';
 import { Roles } from 'nest-keycloak-connect';
-import { ApiBearerAuth } from '@nestjs/swagger';
-import * as dotenv from 'dotenv' 
-dotenv.config({path: './.env'})
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
+dotenv.config({ path: './.env' });
 @Controller('receivers')
+@ApiTags('Recebedores')
 @ApiBearerAuth()
 export class ReceiversController {
   constructor(private readonly receiversService: ReceiversService) {}
 
   @Post()
-  create(@Body() createReceiverDto: CreateReceiverDto) {
-    return this.receiversService.create(createReceiverDto);
+  @Roles({ roles: [`realm:${process.env.KEYCLOAK_CLIENT_ID}-api-create`] })
+  async create(@Body() createReceiverDto: CreateReceiverDto) {
+    return await this.receiversService.create(createReceiverDto);
   }
 
   @Get()
   @Roles({ roles: [`realm:${process.env.KEYCLOAK_CLIENT_ID}-api-read`] })
-  findAll() {
-    return this.receiversService.findAll();
+  async findAll() {
+    return await this.receiversService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.receiversService.findOne(+id);
+  @Roles({ roles: [`realm:${process.env.KEYCLOAK_CLIENT_ID}-api-read`] })
+  async findOne(@Param('id') id: string) {
+    return await this.receiversService.findOne(+id);
   }
 
   @Patch(':id')
-  update(
+  @Roles({ roles: [`realm:${process.env.KEYCLOAK_CLIENT_ID}-api-update`] })
+  async update(
     @Param('id') id: string,
     @Body() updateReceiverDto: UpdateReceiverDto,
   ) {
-    return this.receiversService.update(+id, updateReceiverDto);
+    return await this.receiversService.update(+id, updateReceiverDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.receiversService.remove(+id);
+  @Roles({ roles: [`realm:${process.env.KEYCLOAK_CLIENT_ID}-api-delete`] })
+  async remove(@Param('id') id: string) {
+    return await this.receiversService.remove(+id);
   }
 }
