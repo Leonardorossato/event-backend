8f7401e0ad59f86903964b9a62b9ec306d793f10
Leonardorossato <leonardorossato21@gmail.com>
2023-01-20 10:23:21 -0300

    update auth.login.dto.ts

--- src/auth/dto/auth.login.dto.ts
+++ src/auth/dto/auth.login.dto.ts
@@ -1,3 +1,9 @@
-export class AuthLoginDTO{
-    
+import { ApiProperty } from '@nestjs/swagger';
+
+export class AuthLoginDTO {
+  @ApiProperty({ nullable: false })
+  username!: string;
+
+  @ApiProperty({ nullable: false })
+  password!: string;
 }
