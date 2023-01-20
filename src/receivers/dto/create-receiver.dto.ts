2c9e06430dbcc6d55d6e0af8e35d7c30dfcabc3d
Leonardorossato <leonardorossato21@gmail.com>
2023-01-20 15:06:00 -0300

    update create-receiver.dto.ts

--- src/receivers/dto/create-receiver.dto.ts
+++ src/receivers/dto/create-receiver.dto.ts
@@ -1,1 +1,18 @@
-export class CreateReceiverDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateReceiverDto {
+  @ApiProperty({ nullable: false, type: String, minLength: 3, maxLength: 255 })
+  fullName!: string;
+
+  @ApiProperty({ nullable: false, type: String, minLength: 3, maxLength: 255 })
+  email!: string;
+
+  @ApiProperty({ nullable: false, type: String, minLength: 3, maxLength: 255 })
+  whatsapp!: string;
+
+  @ApiProperty({ nullable: false, type: String })
+  cellphone!: string;
+
+  @ApiProperty({ nullable: false, type: String, minLength: 3, maxLength: 255 })
+  message!: string;
+}
