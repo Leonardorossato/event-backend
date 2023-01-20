7fe174806aef616b5ff0fca779f997c4eac36e78
Leonardorossato <leonardorossato21@gmail.com>
2023-01-20 10:10:15 -0300

    update receiver.entity.ts

--- src/receivers/entities/receiver.entity.ts
+++ src/receivers/entities/receiver.entity.ts
@@ -1,7 +1,10 @@
-import { Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Receiver {
     @PrimaryGeneratedColumn()
     id!: number;
+
+    @Column()
+    name!: string
 }
