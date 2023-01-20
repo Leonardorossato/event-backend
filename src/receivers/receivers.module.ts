1660b3f395548f0979a4feb885e663f2e5b7c21a
Leonardorossato <leonardorossato21@gmail.com>
2023-01-20 15:16:39 -0300

    update receivers.module.ts

--- src/receivers/receivers.module.ts
+++ src/receivers/receivers.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ReceiversService } from './receivers.service';
 import { ReceiversController } from './receivers.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Receiver } from './entities/receiver.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Receiver])],
   controllers: [ReceiversController],
-  providers: [ReceiversService]
+  providers: [ReceiversService],
 })
 export class ReceiversModule {}
