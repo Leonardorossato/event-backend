5aa0f16dc3c9dae84ffe312df12ebe0173c46201
Leonardorossato <leonardorossato21@gmail.com>
2023-01-20 10:20:54 -0300

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ReceiversModule } from './receivers/receivers.module';
 import { KeycloakModule } from './keycloak/keycloak.module';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ReceiversModule, KeycloakModule],
+  imports: [
+    ReceiversModule,
+    KeycloakModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+  ],
 })
 export class AppModule {}
