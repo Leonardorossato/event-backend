2be11faf8de2e932ad96e7b532480e6b9ed9f3e2
Leonardorossato <leonardorossato21@gmail.com>
2023-01-20 10:24:45 -0300

    update auth.service.ts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosError } from 'axios';
+import { AuthLoginDTO } from './dto/auth.login.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async login(dto: AuthLoginDTO) {
+    try {
+      const result = await axios.post(
+        `${process.env.KEYCLOAK_URL}/realms/${process.env.KEYCLOAK_REALM}/protocol/openid-connect/token`,
+        {
+          ...dto,
+          grant_type: 'password',
+          client_id: `${process.env.KEYCLOAK_CLIENT_ID}`,
+          client_secret: `${process.env.KEYCLOAK_SECRET}`,
+          scope: 'openid',
+        },
+        {
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        },
+      );
+      return { access_token: result.data.access_token };
+    } catch (error) {
+      const err = error as AxiosError;
+      return err.message;
+    }
+  }
+}
