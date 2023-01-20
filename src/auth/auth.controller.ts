7e9fc6b9db5344a073702796d2363f58220de280
Leonardorossato <leonardorossato21@gmail.com>
2023-01-20 15:25:06 -0300

    update auth.controller.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,11 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { Public } from 'nest-keycloak-connect';
 import { AuthService } from './auth.service';
 import { AuthLoginDTO } from './dto/auth.login.dto';
 
 @Controller('auth')
+@ApiTags('Autenticação')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
