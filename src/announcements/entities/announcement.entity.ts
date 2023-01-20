1379b6914272d692793ac7e5e931883521061e70
Leonardorossato <leonardorossato21@gmail.com>
2023-01-20 15:37:11 -0300

    update announcement.entity.ts

--- src/announcements/entities/announcement.entity.ts
+++ src/announcements/entities/announcement.entity.ts
@@ -1,1 +1,24 @@
-export class Announcement {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Announcement {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ nullable: false, type: 'varchar', length: 255 })
+  creatorAnnouncement!: string;
+
+  @Column({ nullable: false, unique: true, type: 'varchar', length: 255 })
+  creatorEmail!: string;
+
+  @Column({ nullable: false, type: 'varchar', length: 255 })
+  communiqContent!: string;
+
+  @CreateDateColumn({ type: Date })
+  createdAt?: Date;
+}
