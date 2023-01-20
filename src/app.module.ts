adb3b60b5cc4f8c879da5ad8b606631ab81b245b
Leonardorossato <leonardorossato21@gmail.com>
2023-01-20 15:25:02 -0300

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { KeycloakConfigService } from './keycloak/keycloak.service';
 import * as dotenv from 'dotenv';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostgresSqlConnection } from './config/ormconfig';
+import { AnnouncementsModule } from './announcements/announcements.module';
 dotenv.config({ path: './.env' });
 
 @Module({
@@ -31,6 +32,7 @@ dotenv.config({ path: './.env' });
       cache: true,
     }),
     AuthModule,
+    AnnouncementsModule,
   ],
   providers: [
     {
