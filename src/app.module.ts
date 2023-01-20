a2490134d39ed47fb1e2177135940fbe00c48f51
Leonardorossato <leonardorossato21@gmail.com>
2023-01-20 10:40:54 -0300

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,34 @@ import { ReceiversModule } from './receivers/receivers.module';
 import { KeycloakModule } from './keycloak/keycloak.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard, KeycloakConnectModule, ResourceGuard, RoleGuard } from 'nest-keycloak-connect';
+import { KeycloakConfigService } from './keycloak/keycloak.service';
 
 @Module({
   imports: [
     ReceiversModule,
     KeycloakModule,
+    KeycloakConnectModule.registerAsync({
+      useExisting: KeycloakConfigService,
+      imports: [KeycloakModule]
+    }),
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ResourceGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RoleGuard,
+    },
   ],
 })
 export class AppModule {}
