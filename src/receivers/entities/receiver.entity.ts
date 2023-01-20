c742749a4d6af94967fa74613400553941ae5c91
Leonardorossato <leonardorossato21@gmail.com>
2023-01-20 14:55:39 -0300

    update receiver.entity.ts

--- src/receivers/entities/receiver.entity.ts
+++ src/receivers/entities/receiver.entity.ts
@@ -1,10 +1,30 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class Receiver {
-    @PrimaryGeneratedColumn()
-    id!: number;
+  @PrimaryGeneratedColumn()
+  id!: number;
 
-    @Column()
-    name!: string
+  @Column({ nullable: false, type: 'varchar', length: 255 })
+  fullName!: string;
+
+  @Column({ nullable: false, unique: true, type: 'varchar', length: 255 })
+  email!: string;
+
+  @Column({ nullable: false, type: 'varchar', length: 255 })
+  whatsapp!: string;
+
+  @Column({ nullable: false, type: 'varchar', length: 14 })
+  cellphone!: string;
+
+  @Column({ nullable: false, type: 'varchar', length: 255 })
+  message!: string;
+
+  @CreateDateColumn({ type: Date })
+  createdAt?: Date;
 }
