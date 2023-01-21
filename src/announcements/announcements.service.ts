2e62d878189b577140bea198f0cf8a6b7acbb20c
Leonardorossato <leonardorossato21@gmail.com>
2023-01-21 10:22:59 -0300

    update announcements.service.ts

--- src/announcements/announcements.service.ts
+++ src/announcements/announcements.service.ts
@@ -1,3 +1,4 @@
+import { Receiver } from '@/receivers/entities/receiver.entity';
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -10,6 +11,8 @@ export class AnnouncementsService {
   constructor(
     @InjectRepository(Announcement)
     private readonly announcementRepository: Repository<Announcement>,
+    @InjectRepository(Receiver)
+    private readonly receiverRepository: Repository<Receiver>,
   ) {}
   async create(dto: CreateAnnouncementDto) {
     try {
@@ -40,8 +43,16 @@ export class AnnouncementsService {
     return `This action returns a #${id} announcement`;
   }
 
-  async update(id: number, updateAnnouncementDto: UpdateAnnouncementDto) {
+  async update(id: number, dto: UpdateAnnouncementDto) {
     try {
+      const receiver = await this.announcementRepository.findOneBy({
+        id: dto.receiverId,
+      });
+      if (!receiver)
+        throw new HttpException(
+          `Error to find receiver with id: ${dto.receiverId}`,
+          HttpStatus.NOT_FOUND,
+        );
       const announcement = await this.announcementRepository.findOneBy({
         id: id,
       });
@@ -50,7 +61,7 @@ export class AnnouncementsService {
           `Error to find announcement with id: ${id}`,
           HttpStatus.NOT_FOUND,
         );
-      await this.announcementRepository.update(id, updateAnnouncementDto);
+      await this.announcementRepository.update(id, dto);
       return { message: 'Announcement successfully updated.' };
     } catch (error) {
       throw new HttpException(
@@ -61,6 +72,22 @@ export class AnnouncementsService {
   }
 
   async remove(id: number) {
-    return `This action removes a #${id} announcement`;
+    try {
+      const announcement = await this.announcementRepository.findOneBy({
+        id: id,
+      });
+      if (!announcement)
+        throw new HttpException(
+          `Error to find announcement with id: ${id}`,
+          HttpStatus.NOT_FOUND,
+        );
+      await this.announcementRepository.delete(id);
+      return { message: 'Announcement deleted successfully.' };
+    } catch (error) {
+      throw new HttpException(
+        `Error to remove announcement with id: ${id}`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 }
