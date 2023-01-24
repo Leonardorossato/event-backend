dc905a87e6468360b9ba3683b9b1dc714375c7b0
Leonardorossato <leonardorossato21@gmail.com>
2023-01-24 10:41:54 -0300

    mailmaler config to use the sendgrid variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ReceiversModule } from './receivers/receivers.module';
 import { KeycloakModule } from './keycloak/keycloak.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import {
@@ -10,6 +10,7 @@ import {
   ResourceGuard,
   RoleGuard,
 } from 'nest-keycloak-connect';
+import { MailerModule } from '@nestjs-modules/mailer';
 import { KeycloakConfigService } from './keycloak/keycloak.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostgresSqlConnection } from './config/ormconfig';
@@ -26,10 +27,23 @@ import { AnnouncementsModule } from './announcements/announcements.module';
     TypeOrmModule.forRootAsync(PostgresSqlConnection),
     ConfigModule.forRoot({
       isGlobal: true,
-      expandVariables: true
+      expandVariables: true,
     }),
     AuthModule,
     AnnouncementsModule,
+    MailerModule.forRootAsync({
+      useFactory: async (config: ConfigService) => ({
+        transport: {
+          host: config.get('API_HOST'),
+          secure: false,
+          auth: {
+            user: config.get('API_USER'),
+            pass: config.get('API_PASSWORD'),
+          },
+        },
+      }),
+      inject: [ConfigService],
+    }),
   ],
   providers: [
     {
