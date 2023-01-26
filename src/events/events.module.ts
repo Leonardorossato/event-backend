a9863341ed2d72ee9cc7442242544519d3fb0e02
Leonardorossato <leonardorossato21@gmail.com>
2023-01-26 15:11:43 -0300

    imports another two module for use

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { EventsController } from './events.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Receiver } from '@/receivers/entities/receiver.entity';
+import { Announcement } from '@/announcements/entities/announcement.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Event, Receiver, Announcement])],
   controllers: [EventsController],
-  providers: [EventsService]
+  providers: [EventsService],
 })
 export class EventsModule {}
