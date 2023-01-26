28a83f0b727e2724110c221f5f84b8c2d82fd99a
Leonardorossato <leonardorossato21@gmail.com>
2023-01-26 15:21:02 -0300

    feat: create 1674757234614-events.ts

---
+++ src/migrations/1674757234614-events.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class events1674757234614 implements MigrationInterface {
+    name = 'events1674757234614'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "events" ("id" SERIAL NOT NULL, "receiverId" integer NOT NULL, "announcementId" integer NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_40731c7151fe4be3116e45ddf73" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "events"`);
+    }
+
+}
