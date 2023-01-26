1464649c0829b527203a5c07f9df88762fa6461a
Leonardorossato <leonardorossato21@gmail.com>
2023-01-26 14:39:41 -0300

    feat: create 1674754673690-announcements.ts

---
+++ src/migrations/1674754673690-announcements.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class announcements1674754673690 implements MigrationInterface {
+  name = 'announcements1674754673690';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "announcement" ("id" SERIAL NOT NULL, "creatorAnnouncements" character varying(255) NOT NULL, "releasetTitle" character varying NOT NULL, "creatorsEmail" character varying(255) NOT NULL, "communiquéContent" character varying(255) NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_9104e7437d51a31ece276bb9a57" UNIQUE ("creatorsEmail"), CONSTRAINT "PK_e0ef0550174fd1099a308fd18a0" PRIMARY KEY ("id"))`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "announcement"`);
+  }
+}
