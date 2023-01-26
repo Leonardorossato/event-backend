4e5daacb86ede3557c05ee6cb817787d1db86482
Leonardorossato <leonardorossato21@gmail.com>
2023-01-26 15:01:16 -0300

    update event.entity.ts

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -1,6 +1,8 @@
+import { Announcement } from '@/announcements/entities/announcement.entity';
 import { Receiver } from '@/receivers/entities/receiver.entity';
 import {
   Column,
+  CreateDateColumn,
   Entity,
   JoinColumn,
   OneToMany,
@@ -18,4 +20,17 @@ export class Event {
 
   @Column({ name: 'receiverId' })
   receiverId!: number;
+
+  @OneToMany(() => Announcement, (announcement) => announcement.id)
+  @JoinColumn({ name: 'announcementId' })
+  announcements?: Announcement;
+
+  @Column({ name: 'announcementId' })
+  announcementId!: number;
+
+  @Column({type: 'varchar', unique: true, nullable: false})
+  email!: string[];
+
+  @CreateDateColumn({ type: Date })
+  createdAt!: Date;
 }
