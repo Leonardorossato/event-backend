d94a9ad521de08b211aed13ea94687f26dc9420a
Leonardorossato <leonardorossato21@gmail.com>
2023-02-03 15:15:15 -0300

    update update-event.dto.ts

--- src/events/dto/update-event.dto.ts
+++ src/events/dto/update-event.dto.ts
@@ -3,4 +3,16 @@ import { ApiProperty } from '@nestjs/swagger';
 export class UpdateEventDto {
   @ApiProperty({ nullable: false })
   name!: string;
+
+  @ApiProperty({ nullable: false })
+  body!: string;
+
+  @ApiProperty({ nullable: false })
+  to!: string;
+
+  @ApiProperty({ nullable: false })
+  phone!: string;
+
+  @ApiProperty({ nullable: false })
+  message!: string;
 }
